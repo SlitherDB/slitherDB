159a4f728ae3fe70bf261cfb103d686e3b68a15d
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-05-04 16:24:37 -0400

    parsing echo statement

---
+++ src/SQIL/parser.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+namespace parser
+{
+    public class Parser
+    {
+
+       public static List<string> Parse(List<string> Tokens) {
+            List<string> parsedTokens = new List<string>();
+            Console.WriteLine("Parsing SQIL program");
+            int currentToken = 0;
+            while (currentToken <= Tokens.Count - 1) {
+                string currentTokenStr = Tokens[currentToken];
+                string[] currentTokenArr = currentTokenStr.Split(':');
+                if (currentTokenArr[0] == "STATEMENT") {
+
+                    parsedTokens.Add(currentTokenArr[1]);
+                } else if (currentTokenArr[0] == "TYPE") {
+                    parsedTokens.Add(currentTokenArr[1]);
+                } else if (currentTokenArr[0] == "'"){
+                    parsedTokens.Add(currentTokenArr[0]);
+                    
+                    
+                   
+                } else if (currentTokenArr[0] == "STRING") {
+                    parsedTokens.Add(currentTokenArr[1]);
+                }
+                currentToken++;
+            }
+            return parsedTokens;
+
+       }
+       
+
+    
+    }
+
+}
