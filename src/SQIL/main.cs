3d86f0203c81e682731123789b0195858cb41e84
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-05-04 16:25:30 -0400

    Run Lexer then Parser

--- src/SQIL/main.cs
+++ src/SQIL/main.cs
@@ -1,1 +1,23 @@
+using System;
+using lexer;
+using parser;
+using System.IO;
+using System.Collections.Generic;
+namespace interpreter
+{
+    public class Transpiler
+    {
+        public static void Main(string[] args)
+        {
+            
+            Lexer lexer = new Lexer();
+            Parser parser = new Parser();
+            List<string> programLexed = Lexer.Lex();
+            List<string> programParsed = Parser.Parse(programLexed);
 
+        }
+       
+       
+    
+    }
+}
