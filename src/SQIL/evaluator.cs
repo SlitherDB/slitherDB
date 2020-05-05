70266452c88bfe845393dbe3bafbd27c857f9642
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-05-05 12:11:23 -0400

    Create evaluator.cs

---
+++ src/SQIL/evaluator.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Outputlib;
+namespace evaluator
+{
+    public class Evaluator
+    {
+
+       
+
+
+        public static void evaluate(List<string> Program)
+        {
+            string statement = "null";
+            string type = "null";
+            string param = "null";
+            outputlib output = new outputlib();
+            int tokenIndex = 0;
+            string token = Program[0];
+            while (tokenIndex != Program.Count) {
+        
+                token = Program[tokenIndex];
+                if (token == "echo") {
+                    statement = "echo";
+                } else if (token == "message") {
+                    type = "message";
+                } else if (token == "'") {
+                   
+                    param = Program[tokenIndex + 1];
+                } else if (token == "NEXT") {
+                 
+                    if (statement == "echo") {
+                        
+                        outputlib.echo(type, param);
+                    }
+                }
+                tokenIndex++;
+            }
+            
+            
+            
+        }
+    }
+
+}
