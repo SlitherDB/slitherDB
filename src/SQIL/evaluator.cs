6b969d152e1fe42e19620d777219111f4abb73bd
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-05-07 10:10:06 -0400

    Update evaluator.cs

--- src/SQIL/evaluator.cs
+++ src/SQIL/evaluator.cs
@@ -1,41 +1,63 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
-namespace parser
+using Outputlib;
+namespace evaluator
 {
-    public class Parser
+    public class Evaluator
     {
 
-       public static List<string> Parse(List<string> Tokens) {
-            List<string> parsedTokens = new List<string>();
-            
-            int currentToken = 0;
-            while (currentToken <= Tokens.Count - 1) {
-                string currentTokenStr = Tokens[currentToken];
-                string[] currentTokenArr = currentTokenStr.Split(':');
-                if (currentTokenArr[0] == "STATEMENT") {
-
-                    parsedTokens.Add(currentTokenArr[1]);
-                } else if (currentTokenArr[0] == "TYPE") {
-                    parsedTokens.Add(currentTokenArr[1]);
-                } else if (currentTokenArr[0] == "'"){
-                    parsedTokens.Add(currentTokenArr[0]);
-                    
-                    
-                   
-                } else if (currentTokenArr[0] == "STRING") {
-                    parsedTokens.Add(currentTokenArr[1]);
-                } else if (currentTokenArr[0] == "NEXT") {
-                    parsedTokens.Add("NEXT");
-                }
-                currentToken++;
-            }
-            return parsedTokens;
-
-       }
        
 
-    
+
+        public static void evaluate(List<string> Program)
+        {
+            string statement = "null";
+            string type = "null";
+            string param = "null";
+            outputlib output = new outputlib();
+            int tokenIndex = 0;
+            string token = Program[0];
+            while (tokenIndex != Program.Count) {
+        
+                token = Program[tokenIndex];
+                if (token == "echo") {
+                    statement = "echo";
+                } else if (token == "message") {
+                    type = "message";
+                } else if (token == "'") {
+                   
+                    param = Program[tokenIndex + 1];
+                } else if (token == "nav") {
+                    statement = "nav";
+                } else if (token == "database") {
+                    type = "database";
+                }  else if (token == "create") {
+                    statement = "create";
+                } else if (token == "collection") {
+                    type = "collection";
+                } else if (token == "document") {
+                    type = "document";
+                }
+                else if (token == "NEXT") {
+                 
+                    if (statement == "echo") {
+                        
+                        outputlib.echo(type, param);
+                    }
+                    else if (statement == "nav") {
+                        outputlib.nav(type, param);
+                    }
+                    else if (statement == "create") {
+                        outputlib.create(type, param);
+                    }
+                }
+                tokenIndex++;
+            }
+            
+            
+            
+        }
     }
 
 }
