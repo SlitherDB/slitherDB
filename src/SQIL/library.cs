1070c6d0f07d8bdb7bfada67120f40f505098eef
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-05-08 16:14:17 -0400

    Update library.cs

--- src/SQIL/library.cs
+++ src/SQIL/library.cs
@@ -1,3 +1,5 @@
+
+
 using System;
 using System.IO;
 using System.Text;
@@ -8,6 +10,7 @@ namespace Outputlib
 {
     public class outputlib
     {
+        public static Dictionary<string, string> variables = new Dictionary<string, string>();  
         public static string pathTodb = "null";
         public static string pathToCollection = "NO COLLECTION HAS BEEN NAVIGATED INTO";
         public static string pathToDocument = "NO  HAS BEEN NAVIGATED INTO";
@@ -16,10 +19,11 @@ namespace Outputlib
 
         public static void echo(string type, string parmeter)
         {
-            Console.WriteLine(type);
+   
+            
             if (type == "message") {
                 Console.WriteLine(parmeter);
-            } else if (type == "query") {
+            } else if (type == "queryStr") {
                 
                           string[] documents = Directory.GetFiles(pathToCollection);
             List<string> texts = new List<string>();
@@ -61,7 +65,65 @@ namespace Outputlib
             }
 
         }
+        } else if (type == "default") {
+            
+            Console.WriteLine(variables[parmeter]);
+        } else if (type == "query") {
+            Console.WriteLine(variables[parmeter]);
         }
+        }
+        public static void var(string type, string parameter) {
+            
+            if (type == "default") {
+                
+                string[] varsplit = parameter.Split('=');
+                string name = varsplit[0];
+                string value = varsplit[1];
+                variables.Add(name, value);
+                
+            } else if (type == "query") {
+                
+                string[] varsplit = parameter.Split('=');
+                string name = varsplit[0];
+                string value = varsplit[1];
+                string[] documents = Directory.GetFiles(pathToCollection);
+                List<string> texts = new List<string>();
+
+                for (int i = 0; i < documents.Length; i++)
+                {
+                
+
+                    string path = Path.Combine(pathToCollection, documents[i]);
+                    string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                    Directory.SetCurrentDirectory(desktop);
+                    string text = System.IO.File.ReadAllText(path);
+                    
+                    if (documents[i] != "/Users/home/Desktop/db/data/.DS_Store")
+                    {
+
+                        if (text.Contains(value))
+                        {
+                            texts.Add(text);
+                            if (i == documents.Length - 1)
+                            {
+                                if (texts.Count == 0)
+                                {
+                                    
+                                    texts.Add("null");
+                                }
+                             
+                                    
+                                    variables.Add(name, string.Join(Environment.NewLine + Environment.NewLine, texts.ToArray()));
+                                    
+                                
+                            }
+                        }
+                    
+
+                }
+
+            }
+            }
         }
         public static void nav(string type, string parameter) {
         
